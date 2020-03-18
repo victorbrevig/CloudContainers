aec08866ef16d0b2496e4eda29aa5c120cfe2467
Bruger <Bruger@LAPTOP-IJDG0SGM.home>
2020-03-18 15:11:56 +0100

    Yo

---
+++ src/main/java/CloudContainers/Database.java
@@ -0,0 +1,39 @@
+package CloudContainers;
+
+import java.util.ArrayList;
+
+public class Database {
+		ArrayList<Client> arr = new ArrayList<Client>();
+		int count = 0;
+		
+		
+		public ArrayList<Client> getArr() {
+			return arr;
+		}
+		
+		public void addClient(Client c) {
+			arr.add(count, c);
+			count++;
+		}
+		
+		public boolean exist(Client c) {
+			boolean b;
+			b = false;
+			for (Client cl : arr) {
+				b = (c.getEmail() == cl.getEmail()) || b;
+			}
+			return b;
+		}
+// public static void main(String[] args) {
+//	Client client;
+//	client = new Client("Bob1","001","bigman1@dtu.dk","22","male","10101010");
+//	Database database = new Database();
+//	database.addClient(client);
+//	database.getArr().get(0);
+//	
+//	for(Client c  : database.arr) {
+//		System.out.println(c.getEmail());
+//	}
+//	
+//}
+}
